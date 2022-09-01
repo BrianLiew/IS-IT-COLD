076a594c90230042ecf281142cffc819d750d347
Brian Liew <brian@BIGMAC-AIR.fios-router.home>
2022-09-01 02:23:32 -0400

    Reworked NetworkingManager

--- is it cold/is it cold/Networking/NetworkingManager.swift
+++ is it cold/is it cold/Networking/NetworkingManager.swift
@@ -1,33 +1,36 @@
 import Foundation
 import CoreLocation
 
-class NetworkingManager {
+struct NetworkingManager {
     
-    private static let key: String = "865e25bdadd4ab58522a489eed0685de"
-    private static var url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely,alerts&appid=\(key)")!
-    
-    static var latitude: Double = 00.00
-    static var longitude: Double = 00.00
-    
-    static let session = URLSession(configuration: URLSessionConfiguration.default)
+    // openweather.org api access key
+    private static let strKey: String = "865e25bdadd4ab58522a489eed0685de"
+    private static let session = URLSession(configuration: URLSessionConfiguration.default)
     
     static func makeRequest(latitude: Double, longitude: Double, completion_handler: @escaping (Data?) -> Void) {
-        self.latitude = latitude
-        self.longitude = longitude
-                
+        // initialize URL
+        let strLatitude: String = String(latitude)
+        let strLongitude: String = String(longitude)
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(strLatitude)&lon=\(strLongitude)&exclude=minutely,alerts&appid=\(strKey)")
+        else {
+            NSLog("NetworkingManager request | URL cannot be initialized from string, app exited with code 0")
+            exit(0)
+        }
+        // make network call
         DispatchQueue.global().async {
-            let task = session.dataTask(with: self.url) { data, response, error in
-                guard let http_response = response as? HTTPURLResponse,
-                      (200...299).contains(http_response.statusCode) else {
-                          if let error_unwrapped = error { print("http response error: \(error_unwrapped.localizedDescription)") }
-                          return
-                      }
-                // check if data is JSON
-                guard let mime = response?.mimeType, mime == "application/json" else {
-                    if let error_unwrapped = error { print("MIME error: \(error_unwrapped.localizedDescription)") }
-                    return
+            let task = session.dataTask(with: url) { data, response, error in
+                // checks for correct HTTPURLResponse.statusCode
+                guard (response as? HTTPURLResponse)?.statusCode == 200
+                else {
+                    NSLog("NetworkingManager request(:Double, :Double, (Data?)->Void) | HTTPURLResponse.status code != 200, app exited with code 0")
+                    exit(0)
                 }
-                if let data_unwrapped = data { completion_handler(data_unwrapped) }
+                
+                if let data = data { completion_handler(data) }
+                else {
+                    NSLog("NetworkingManager request(:Double, :Double, (Data?)->Void) | Data could not be unwrapped to be used in completion handler, app exited with code 0")
+                    exit(0)
+                }
             }
             task.resume()
         }
